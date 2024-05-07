1a98aaa9347245e43fb3024e6602cfaf493c9446
SyncContent <34035324+SyncContent@users.noreply.github.com>
2024-05-07 08:41:15 -0400

    initial commit part one

---
+++ ImageClassification/ImgClass/ImgClass/ImageClassification.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace ImgClass
+{
+    public class ImageClassification
+    {
+        public class ModelInput
+        {
+            [LoadColumn(0)]
+            [ColumnName(@"Label")]
+            public string Label { get; set; }
+
+            [LoadColumn(1)]
+            [ColumnName(@"ImageSource")]
+            public byte[] ImageSource { get; set; }
+
+        }
+
+        public class ModelOutput
+        {
+            [ColumnName(@"Label")]
+            public uint Label { get; set; }
+
+            [ColumnName(@"ImageSource")]
+            public byte[] ImageSource { get; set; }
+
+            [ColumnName(@"PredictedLabel")]
+            public string PredictedLabel { get; set; }
+
+            [ColumnName(@"Score")]
+            public float[] Score { get; set; }
+
+        }
+
+        private static string MLNetModelPath = Path.GetFullPath("ImgClass.zip");
+
+        public static readonly Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(() => CreatePredictEngine(), true);
+
+        private static PredictionEngine<ModelInput, ModelOutput> CreatePredictEngine()
+        {
+            var mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load(MLNetModelPath, out var _);
+            return mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+        }
+
+        public static ModelOutput Predict(ModelInput input)
+        {
+            var predEngine = PredictEngine.Value;
+            return predEngine.Predict(input);
+        }
+
+        public static IEstimator<ITransformer> BuildPipeline(MLContext mlContext)
+        {
+            // Data process configuration with pipeline data transformations
+            var pipeline = mlContext.Transforms.Conversion.
+                MapValueToKey(
+                    outputColumnName: @"Label", inputColumnName: @"Label")
+            .Append(mlContext.MulticlassClassification.
+                Trainers.ImageClassification(
+                    labelColumnName: @"Label", 
+                    scoreColumnName: @"Score", 
+                    featureColumnName: @"ImageSource")
+            )
+            .Append(mlContext.Transforms.Conversion.
+                MapKeyToValue(outputColumnName: @"PredictedLabel", 
+                inputColumnName: @"PredictedLabel")
+            );
+
+            return pipeline;
+        }
+    }
+}
---
+++ ImageClassification/ImgClass/ImgClass/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ImgClass
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Load sample data
+            var imageBytes = File.ReadAllBytes(@"C:\PERSONAL-DATA\Projects\Books\ML.NET Succinctly\ML.NET\sample-images\Broccoli\bowl-of-broccoli-2584307_640.jpg");
+            ImageClassification.ModelInput sampleData = new ImageClassification.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+
+            //Load model and predict output
+            var result = ImageClassification.Predict(sampleData);
+            Console.WriteLine($"Category {result.PredictedLabel}");
+        }
+    }
+}
